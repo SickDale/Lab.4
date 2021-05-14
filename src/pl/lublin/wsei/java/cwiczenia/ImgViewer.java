2d5ab7aa76b98fe3ec98f7ef44ce04eb67933d6e
Daniel72674 <56113994+Daniel72674@users.noreply.github.com>
2021-05-14 20:38:47 +0200

    Lab 4b| Punkt 7 (ostatni) Wszystko działa ,z wyjątkiem tego,że po naciśnięciu przycisku "pokaż" w podglądzie infografiki nie pokazuje się żadna grafika .Robiłem zgodnie z instrukacją w Laboratoriach ,ale niestety nie widzę żadnego błędu

---
+++ src/pl/lublin/wsei/java/cwiczenia/ImgViewer.java
@@ -0,0 +1,8 @@
+package pl.lublin.wsei.java.cwiczenia;
+
+import javax.swing.text.html.ImageView;
+
+public class ImgViewer {
+    public ImageView imgView;
+    public javafx.scene.image.ImageView ImgView;
+}
